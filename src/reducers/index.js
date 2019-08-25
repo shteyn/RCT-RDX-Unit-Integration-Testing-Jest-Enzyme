c32679a2a5217b8c3ab2c7c4919cd8c15f3232be
Anna <anna@Dyss-MacBook-Pro.local>
2019-08-25 17:31:58 +0200

    Posts reducer with tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import success from "./successReducer.js";
+import posts from "./posts/reducer";
 
 export default combineReducers({
-  success
+  posts
 });
